1c638af0aaece0fa83b9e407635f3fba0761e9a7
vincentianxing <sky103034@gmail.com>
2024-04-22 14:04:25 -0400

    update demo

--- run_asr.sh
+++ run_asr.sh
@@ -35,14 +35,16 @@
 
 # sbatch --partition=gpus --gres=gpu:4 --mem=16G run_asr.sh
 
-echo "1"
-python demo_asr.py --video_example=./videos/1.mp4 --asr_example ./output_asr/1.pkl --combine_datasets chapters
-echo "2"
-python demo_asr.py --video_example=./videos/2.mp4 --asr_example ./output_asr/2.pkl --combine_datasets chapters
-echo "3"
-python demo_asr.py --video_example=./videos/3.mp4 --asr_example ./output_asr/3.pkl --combine_datasets chapters
-echo "4"
-python demo_asr.py --video_example=./videos/4.mp4 --asr_example ./output_asr/4.pkl --combine_datasets chapters
-echo "5"
-python demo_asr.py --video_example=./videos/5.mp4 --asr_example ./output_asr/5.pkl --combine_datasets chapters
-echo "done"
+# echo "1"
+# python demo_asr.py --video_example=./videos/1.mp4 --asr_example ./output_asr/1.pkl --combine_datasets chapters
+# echo "2"
+# python demo_asr.py --video_example=./videos/2.mp4 --asr_example ./output_asr/2.pkl --combine_datasets chapters
+# echo "3"
+# python demo_asr.py --video_example=./videos/3.mp4 --asr_example ./output_asr/3.pkl --combine_datasets chapters
+# echo "4"
+# python demo_asr.py --video_example=./videos/4.mp4 --asr_example ./output_asr/4.pkl --combine_datasets chapters
+# echo "5"
+# python demo_asr.py --video_example=./videos/5.mp4 --asr_example ./output_asr/5.pkl --combine_datasets chapters
+# echo "done"
+
+python demo_asr.py --video_example=./videos/6.mp4 --asr_example ./output_asr/6.pkl --combine_datasets chapters
